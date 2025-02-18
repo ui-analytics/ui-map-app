1c8e2d89b76d358ec83b30dca953332a9b3dc222
Nick Occhipinti <nocchipi@charlotte.edu>
2025-02-18 10:14:48 -0500

    code cleanup

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnChanges, OnInit, signal, ViewChild, viewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, OnInit, signal, ViewChild, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonToggleModule, MatButtonToggleGroup } from '@angular/material/button-toggle'
@@ -10,65 +10,79 @@ import { ToolbarItemComponent } from "./toolbar-item/toolbar-item.component";
 import { MapVariable } from '../shared/models/map-variable';
 import { ToolbarVariableComponent } from "./toolbar-variable/toolbar-variable.component";
 
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-toolbar',
   imports: [MatToolbarModule, CommonModule, ToolbarItemComponent, MatButtonToggleModule, ToolbarVariableComponent],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements AfterViewInit {
 
-  constructor(private mapService: MapService) { }
+  private categorySubscription:Subscription;
+  private variableSubscription:Subscription;
+
+  constructor(private mapService: MapService, private cdr: ChangeDetectorRef) { 
+    this.categorySubscription = this.mapService.getCurrentCategory().subscribe((value) => {
+      // console.log('current category is:',value)
+      this.selectedCategory = value;
+      this.variableList = this.selectedCategory.mapVariables;
+      // console.log(' map variables available are', this.variableList)
+    });
+
+    this.variableSubscription = this.mapService.getCurrentVariable().subscribe((value) => {
+      // console.log('current variable is:',value)
+      this.selectedVariable = value;
+    });
+
+  }
 
   @Input() toolbar_name: string = '';
   categories: MapCategory[] = [];
+  selectedCategory?: MapCategory;
+  variableList: Number[] =  [];
+
   variables: MapVariable[] = [];
+  selectedVariable?: MapVariable;
+
   hideSelection = signal(false);
 
-  defaultValue:String = 'Demographics';
 
   getCategories(): void {
       this.mapService.getMapCategories()
       .subscribe(mc => {
-        this.mapService.mapCategories = mc
+        this.categories = mc
       })
     }
 
-  getVariables(): void {
-    this.mapService.getMapVariables()
-    .subscribe(mv => {
-      this.mapService.mapVariables = mv
-    })
-  }
-
-  ngOnInit(): void {
-    this.getCategories()
-    this.categories = this.mapService.mapCategories
-    if (this.categories.length > 0){
-      this.mapService.selectedCategory = this.mapService.mapCategories[0]
-      this.getVariables()
-      this.variables = this.mapService.mapVariables
-      if (this.variables.length > 0) {
-        this.mapService.selectedVariable = this.mapService.mapVariables[0]
-      }
+    getVariableList(): void {
+      this.mapService.getMapVariables(this.variableList).subscribe((mv) => {
+        this.variables = mv;
+      })
     }
+
+    ngAfterViewInit(): void {
+      console.log('t');
+      this.getCategories();
+      this.getVariableList();
+      // this.cdr.detectChanges();
   }
 
   onToggleGroupChange(event:any) {
     let selectedValue = event.value
-    this.mapService.selectedCategory = this.categories.filter((cat) => cat.name === selectedValue).reduce((acc: any, it) => it, { })
-    this.getVariables()
-    this.variables = this.mapService.mapVariables
-      if (this.variables.length > 0) {
-        this.mapService.selectedVariable = this.mapService.mapVariables[0]
-        this.mapService.updateCurrentVariable(this.mapService.selectedVariable);
-      }
+    let selectedCategory = this.categories.filter((cat) => cat.name === selectedValue).reduce((acc: any, it) => it, { })
+    this.mapService.updateCurrentCategory(selectedCategory);
+    this.getVariableList();
+    this.mapService.updateCurrentVariable(this.variables[0]);
+    
   }
 
   onToggleVariableChange(event:any) {
     let selectedValue = event.value
-    this.mapService.selectedVariable = this.variables.filter((cat) => cat.name === selectedValue).reduce((acc: any, it) => it, { })
-    this.mapService.updateCurrentVariable(this.mapService.selectedVariable);
+    let currentVariable = this.variables.filter((cat) => cat.name === selectedValue).reduce((acc: any, it) => it, { })
+    this.mapService.updateCurrentVariable(currentVariable);
+    
   }
 
   
