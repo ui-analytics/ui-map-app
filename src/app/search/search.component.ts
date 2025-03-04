91905bf6300011bc85a560cad57af5476f71f44e
Nick Occhipinti <nocchipi@charlotte.edu>
2025-03-04 15:45:47 -0500

    when option is selected zoom to area and highlight

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,6 +10,13 @@ import { Extent } from '../shared/models/extent';
 import { MapService } from '../services/map.service';
 import { MatOptionSelectionChange } from '@angular/material/core';
 
+import { Extent as EsriExtent } from "@arcgis/core/geometry";
+import Query from "@arcgis/core/rest/support/Query.js";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
+import Graphic from "@arcgis/core/Graphic.js";
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol.js";
+import ColorVariable from "@arcgis/core/renderers/visualVariables/ColorVariable.js";
+
 @Component({
   selector: 'app-search',
   imports: [FormsModule,
@@ -48,6 +55,51 @@ export class SearchComponent implements OnInit {
 
   onSelectionChange(event: MatOptionSelectionChange, option:Extent) {
     console.log('selected:',option);
+    // TODO: find better way to remove graphics layers
+    this.mapService.map.layers.forEach((layer) => {
+      if (layer.type === "graphics") {
+        this.mapService.map.remove(layer);
+      }
+    });
+
+    let extent = new EsriExtent({
+      xmin: option.xmin,
+      ymin: option.ymin,
+      xmax: option.xmax,
+      ymax: option.ymax,
+      spatialReference: {wkid: option.wkid}
+    })
+
+    this.mapService.mapView.goTo(extent);
+
+    const query = new Query();
+    query.where = `crdt_unique_id = '${option.crdt_unique_id}'`;
+    // query.outSpatialReference = { wkid: option.wkid };
+    query.returnGeometry = true;
+    this.mapService.featureLayer.queryFeatures(query).then((results) => {
+      const graphicsLayer = new GraphicsLayer();
+      const graphics = results.features.map((feature) => {
+        return new Graphic({
+          geometry: feature.geometry,
+          attributes: feature.attributes,
+          symbol: new SimpleFillSymbol({
+            color: [0, 0, 255, 0], // Blue with 30% transparency
+            outline: {
+              color: [0, 255, 255, 1], // Blue outline
+              width: 3
+            }
+          })
+        })
+      })
+
+      graphicsLayer.addMany(graphics);
+      this.mapService.map.add(graphicsLayer);
+      
+    }).catch((error)=> console.error('failed',error));
+  }
+
+  onFormClick(event:any) {
+    this.searchControl.reset();
   }
 
 }
