d100e0faf31dc8971ec8ff4c52f862993f818924
Nick Occhipinti <nocchipi@charlotte.edu>
2025-03-27 07:19:15 -0400

    updated categories and variables

--- src/app/shared/mocks/mock-map-category.ts
+++ src/app/shared/mocks/mock-map-category.ts
@@ -4,26 +4,46 @@ export const MAP_CATEGORY: MapCategory[] = [
     {
         categoryId: 1,
         name: 'Character',
-        mapVariables: [1,2,3,4,5,6,7,8]
+        mapVariables: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     },
     {
         categoryId: 2,
-        name: 'Education',
-        mapVariables: [9,10,11]
+        name: 'Economy',
+        mapVariables: [13, 14, 15, 16, 17, 18, 19, 20, 21]
     },
     {
         categoryId: 3,
-        name: 'Economy',
-        mapVariables: [12,13]
+        name: 'Education',
+        mapVariables: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
     },
     {
         categoryId: 4,
-        name: 'Housing',
-        mapVariables: [14,15,16]
+        name: 'Engagement',
+        mapVariables: [34, 35, 36, 37]
     },
     {
         categoryId: 5,
+        name: 'Environment',
+        mapVariables: [38, 39,40, 41, 42, 43, 44, 45, 46, 47, 48]
+    },
+    {
+        categoryId: 6,
+        name: 'Health',
+        mapVariables: [49, 50, 51, 52, 53, 54, 55, 56, 57]
+    },
+    {
+        categoryId: 7,
+        name: 'Housing',
+        mapVariables: [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73]
+    },
+    {
+        categoryId: 8,
+        name: 'Safety',
+        mapVariables: [74, 75,76, 77, 78, 79]
+    },
+    {
+        categoryId: 9,
         name: 'Transportation',
-        mapVariables: [17,18,19]
+        mapVariables: [80, 81,82, 83, 84, 85, 86, 87]
     }
 ];
